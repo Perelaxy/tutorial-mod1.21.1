c99fdc089ad2b36e75d3410171b8e83e1d532c17
Perelaxy <tlandberg102@gmail.com>
2024-08-22 16:47:28 +0200

    yep

---
+++ src/main/java/net/perelaxy/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.perelaxy.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
